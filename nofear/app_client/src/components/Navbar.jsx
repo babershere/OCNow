5b4c6b1d2ce564843c9c90b08dbac55e14c285b7
Adrian <adrian88.an@gmail.com>
2018-03-13 18:35:57 -0700

    revised the navbar

--- nofear/app_client/src/components/Navbar.jsx
+++ nofear/app_client/src/components/Navbar.jsx
@@ -4,33 +4,33 @@ import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchTerm: '',
-            userPassword: '',
-            userName: ''
-        };
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         searchTerm: '',
+    //         userPassword: '',
+    //         userName: ''
+    //     };
     
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //   }
     
-      handleChange(event) {
-        const inputName = event.target.name;
-        this.setState({[inputName]: event.target.value});
-        console.log(this.state)
-      }
+    //   handleChange(event) {
+    //     const inputName = event.target.name;
+    //     this.setState({[inputName]: event.target.value});
+    //     console.log(this.state)
+    //   }
     
-      handleSubmit(event) {
-        event.preventDefault();
-        this.setState({
-            searchTerm: '',
-            userPassword: '',
-            userName: ''
-        }) 
+    //   handleSubmit(event) {
+    //     event.preventDefault();
+    //     this.setState({
+    //         // searchTerm: '',
+    //         userPassword: '',
+    //         userName: ''
+    //     }) 
 
-      }
+    //   }
     
 
     render() {
@@ -62,30 +62,30 @@ class Navbar extends React.Component {
                     </li>   
                 </ul>
 
-                <div className="navbar-form navbar-left">
+                {/* <div className="navbar-form navbar-left">
                 <form className="navbar-search pull-right" onSubmit={this.handleSubmit}>
                     <input name="searchTerm" type="text" className="search-query form-control" placeholder="Search" value={this.state.searchTerm} onChange={this.handleChange} />
                 </form> 
-                </div>
+                </div> */}
 
                 <div className="navbar-form navbar-right">                
                     <form className="navbar-search" >
                         <div className="form-group" onSubmit={this.handleSubmit}>
-                            <input name="userName" type="text" className="form-control" placeholder="User Name" value={this.state.userName} onChange={this.handleChange} />
+                            {/* <input name="userName" type="text" className="form-control" placeholder="User Name" value={this.state.userName} onChange={this.handleChange} />
                             <input name="userPassword" type="password" className="form-control" placeholder="Password" value={this.state.userPassword} onChange={this.handleChange} />                    
-                            <button className="btn btn-primary" disabled="">Log In</button>
+                            <button className="btn btn-primary" disabled="">Log In</button> */}
                             <button className="btn btn-danger" disabled="">Log Out</button>
                         </div>
                     </form> 
                 </div>
 
-                <div className="navbar-form navbar-right">                
+                {/* <div className="navbar-form navbar-right">                
                     <form className="navbar-search pull-right" >
                         <div className="form-group" onSubmit={this.handleSubmit}>
                             <button className="btn btn-success" disabled="">Sign Up</button>
                         </div>
                     </form> 
-                </div>
+                </div> */}
             </div>         
         </nav>
         )
