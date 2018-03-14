87ca3b900d259d78652e5a4fa78522fd4336eaa0
babershere <rehmatq@gmail.com>
2018-03-14 00:23:13 -0700

    log in compenent returns back with status ok

--- nofear/app_client/src/components/users/SignUpForm.jsx
+++ nofear/app_client/src/components/users/SignUpForm.jsx
@@ -13,7 +13,28 @@ class SignUpForm extends Component {
             email: "",
             password: ""
         }
-    
+        this.submit = this.submit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);    
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    submit(e) {
+        e.preventDefault();
+        axios.post("/signup", this.state)
+            .then(resp => {
+                console.log(resp);
+                this.props.history.push("/login");
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
 
     render() {
@@ -27,22 +48,22 @@ class SignUpForm extends Component {
                     {/*  PENDING TO MODIFY ON CHANGE */}
                     <div className="form-group">
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text" className="form-control" name="firstName" placeholder="First Name..." onChange={this.props.handleChange}/>
+                        <input type="text" className="form-control" name="firstName" placeholder="First Name..." onChange={this.handleInputChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text" className="form-control" name="lastName" placeholder="Last Name..." onChange={this.props.handleChange}/>
+                        <input type="text" className="form-control" name="lastName" placeholder="Last Name..." onChange={this.handleInputChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" name="email" placeholder="Email..." onChange={this.props.handleChange}/>
+                        <input type="email" className="form-control" type="email" name="email" placeholder="Email..." onChange={this.handleInputChange}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="Password" placeholder="********"  onChange={this.props.handleChange}/>
+                        <input type="password" className="form-control" type="password" name="Password" placeholder="********"  onChange={this.handleInputChange}/>
                     </div>
 
-                    <button type="button" className="btn btn-default navbar-btn">Sign Up!</button>
+                    <button type="button" className="btn btn-default navbar-btn" onClick={this.submit}>Sign Up!</button>
 
                     {/* <div className="clearfix">
                         <FormBtns handleDelete = {this.props.handleDelete} 
@@ -52,19 +73,6 @@ class SignUpForm extends Component {
                     </div> */}
 
                 </form>
-                <div className="form-group" onSubmit={this.handleSubmit}>
-                    <a className="btn btn-danger" onClick={
-                        (e) => {
-                            e.preventDefault();
-                            console.log("sup");
-                            axios.get("/auth/google")
-                                .then(function (resp) {
-                                    console.log(resp)
-                                })
-
-                        }
-                    } >Google +</a>
-                </div>
             </div>
         )
     }
