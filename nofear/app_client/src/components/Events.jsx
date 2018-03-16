4379b467ee227cef0f25d95b0816eb48c4f0b2d5
Sumaira Memon <sumairamemon@dhcp-v073-231.mobile.uci.edu>
2018-03-15 19:16:00 -0700

    conflicts

--- nofear/app_client/src/components/Events.jsx
+++ nofear/app_client/src/components/Events.jsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import Events from "react"; 
+//import React from "react";
+//import Events from "react"; 
 
 
-// class Events extends React.Component {
+//class Events extends React.Component {
 
    
 
-//     render() {
-//         return (
-//         <h1><div w-type="event-discovery" w-tmapikey="FYB1xd7dcO9vRp8AQxJ8bjdn1fdrl7iH" w-googleapikey="AIzaSyDFkntugH-pXh5GT9tCLj10gqX1yNgC6z0" w-keyword="" w-theme="listviewthumbnails" w-colorscheme="light" w-width="350" w-height="600" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="25" w-period="month" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-countrycode="US" w-source="" w-latlong=""></div></h1>
-//         )
-//     }
-// }  
+  //  render() {
+        //return (
+        //<h1>events</h1>
+  //      )
+  //  }
+//}  
 
     
 
 
 
 
-// export default Events; 
+//export default Events; 
